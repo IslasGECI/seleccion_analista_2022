02ea8f28554c863b296abf40e63163c6e9281b9d
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-02-08 19:57:58 -0800

    🧪 Add failing test to define new function

--- tests/test_dummy_model.py
+++ tests/test_dummy_model.py
@@ -1,4 +1,11 @@
+from ..pollos_petrel import read_trainig_dataset
+
+
 # Lee train.csv
+def test_read_training_dataset():
+    read_trainig_dataset()
+
+
 # Calcula promedio de target
 # Lee test.csv
 # Tira todas las columnas excepto id
