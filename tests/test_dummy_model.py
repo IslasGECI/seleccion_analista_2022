bda2091d9b41434b12892603c4edd05a5b8c55eb
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-02-09 11:19:56 -0800

    🧪 Add failing test to implement function get_mean_target()

--- tests/test_dummy_model.py
+++ tests/test_dummy_model.py
@@ -1,4 +1,5 @@
 from pollos_petrel import get_mean_target, read_trainig_dataset
+import pandas as pd
 
 
 # Lee train.csv
@@ -11,7 +12,11 @@ def test_read_training_dataset():
 
 # Calcula promedio de target
 def test_get_mean_target():
-    get_mean_target()
+    data = {"id": [1, 2], "target": [3, 4]}
+    dataset = pd.DataFrame(data=data)
+    obtained_mean = get_mean_target(dataset)
+    expected_mean = 3.5
+    assert expected_mean == obtained_mean
 
 
 # Lee test.csv
