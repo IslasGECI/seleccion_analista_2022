395818c36680910eaa77a6a6cf8ab10c16459744
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-02-10 10:23:38 -0800

    ✅ Implement read_testing_dataset()

--- R/dummy_model.R
+++ R/dummy_model.R
@@ -5,3 +5,10 @@ read_training_dataset <- function() {
     training_dataset = read_csv(training_dataset_path)
     return(training_dataset)
 }
+
+
+read_testing_dataset <- function() {
+    testing_dataset_path <- "pollos_petrel/test.csv"
+    testing_dataset = read_csv(testing_dataset_path)
+    return(testing_dataset)
+}
