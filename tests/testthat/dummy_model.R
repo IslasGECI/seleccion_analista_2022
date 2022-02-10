c0e3a5a3df0da88df08d1f723a250d74da3bae73
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-02-10 10:12:29 -0800

    🧪 Add failing test to implement read_training_dataset()

--- tests/testthat/dummy_model.R
+++
@@ -1,6 +0,0 @@
-# Lee train.csv
-# Calcula promedio de target
-# Lee test.csv
-# Tira todas las columnas excepto id
-# Agrega columna target con el promedio
-# Guarda el archivo con sufijo _submission.csv
---
+++ tests/testthat/test_dummy_model.R
@@ -0,0 +1,18 @@
+setwd("/workdir")
+
+# Lee train.csv
+describe("Read training dataset", {
+  it("reads all 1304 records", {
+    training_dataset <- read_training_dataset()
+    obtained_n_rows <- dim(training_dataset)[1]
+    expected_n_rows <- 1304
+    expect_equal(expected_n_rows, obtained_n_rows)
+  })
+})
+
+
+# Calcula promedio de target
+# Lee test.csv
+# Tira todas las columnas excepto id
+# Agrega columna target con el promedio
+# Guarda el archivo con sufijo _submission.csv
