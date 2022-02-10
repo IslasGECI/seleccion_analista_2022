cef8eea0d6d1bc7d1e4fc8db53ee4f3ca9edd3a7
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-02-09 20:13:48 -0800

    😅 Add empty test file

---
+++ tests/testthat/dummy_model.R
@@ -0,0 +1,6 @@
+# Lee train.csv
+# Calcula promedio de target
+# Lee test.csv
+# Tira todas las columnas excepto id
+# Agrega columna target con el promedio
+# Guarda el archivo con sufijo _submission.csv
