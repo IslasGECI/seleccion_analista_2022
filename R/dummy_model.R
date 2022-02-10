d5d3325a06967db67907bc1d0bccb0d709ddc2c3
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-02-10 10:38:38 -0800

    🎨🇷 Format with styler

--- R/dummy_model.R
+++ R/dummy_model.R
@@ -1,14 +1,14 @@
 library(tidyverse)
 
 read_training_dataset <- function() {
-    training_dataset_path <- "pollos_petrel/train.csv"
-    training_dataset = read_csv(training_dataset_path)
-    return(training_dataset)
+  training_dataset_path <- "pollos_petrel/train.csv"
+  training_dataset <- read_csv(training_dataset_path)
+  return(training_dataset)
 }
 
 
 read_testing_dataset <- function() {
-    testing_dataset_path <- "pollos_petrel/test.csv"
-    testing_dataset = read_csv(testing_dataset_path)
-    return(testing_dataset)
+  testing_dataset_path <- "pollos_petrel/test.csv"
+  testing_dataset <- read_csv(testing_dataset_path)
+  return(testing_dataset)
 }
